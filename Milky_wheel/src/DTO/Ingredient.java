77e548be3f173a275e6c37d046434341619e4a1b
Charlottelbaker <62942187+Charlottelbaker@users.noreply.github.com>
2025-11-01 15:20:30 +0000

    ingredeint

---
+++ Milky_wheel/src/DTO/Ingredient.java
@@ -0,0 +1,16 @@
+package DTO;
+
+public class Ingredient {
+    private String name;
+    private double quantity;
+    private String unit;
+
+    public Ingredient(String name, double quantity, String unit) {
+        this.name = name;
+        this.quantity = quantity;
+        this.unit = unit;
+
+
+    }
+
+}
--- Milky_wheel/src/DTO/Recipe.java
+++ Milky_wheel/src/DTO/Recipe.java
@@ -1,2 +1,4 @@
+package DTO;
+
 public class Recipe {
 }
